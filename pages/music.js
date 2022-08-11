56b857de4e2b7702f0acd86f2a82b79d3a5b343c
kwangric <43795498+kwangric@users.noreply.github.com>
2022-08-11 14:19:34 -0400

    Add link to music title

--- pages/music.js
+++ pages/music.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import SongList from '../components/SongList'
 import musicStyles from '../styles/Music.module.css'
-import {songs} from '../assets/songs'
+import { songs } from '../assets/songs'
 
-const music = ({songs}) => {
+const music = ({ songs }) => {
   return (
     <div>
       <Head>
@@ -13,10 +13,20 @@ const music = ({songs}) => {
           content="web development, software engineer, programming, richard kwang"
         />
       </Head>
-      <h1 className={musicStyles.musicTitle}>McQueen</h1>
-      <hr className={musicStyles.musicLine}/>
-      <p className={musicStyles.subtitle}>All songs written and produced by Richard Kwang</p>
-      <SongList songs={songs}/>
+      <div className={musicStyles.musicTitleContainer}>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://open.spotify.com/artist/6lohbb4AT5dqX3bPzfhjra?si=TyOYQdIwTa2X26rsOAG62w"
+      >
+        <h1 className={musicStyles.musicTitle}>McQueen</h1>
+      </a>
+      </div>
+      <hr className={musicStyles.musicLine} />
+      <p className={musicStyles.subtitle}>
+        All songs written and produced by Richard Kwang
+      </p>
+      <SongList songs={songs} />
     </div>
   )
 }
@@ -24,8 +34,8 @@ const music = ({songs}) => {
 export const getStaticProps = async () => {
   return {
     props: {
-      songs: songs
-    }
+      songs: songs,
+    },
   }
 }
 
