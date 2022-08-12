1488e9bc9f62a0560941aa7cc93782473b543ff9
kwangric <43795498+kwangric@users.noreply.github.com>
2022-08-12 13:18:57 -0400

    Added meta data

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,14 @@ export default function Home({ articles }) {
   return (
     <div>
       <Head>
+        <link rel="canonical" href="https://www.richardkwang.com" />
+        <meta property="og:url" content="https://www.richardkwang.com" />
+
         <title>Richard Kwang</title>
+        <meta property="og:title" content="Richard Kwang" />
+        <meta name="description" content="Coding is my passion" />
+        <meta property="og:description" content="Coding is my passion" />
+        <meta property="og:type" content="article" />
         <meta
           name="keywords"
           content="web development, software engineer, programming, richard kwang"
