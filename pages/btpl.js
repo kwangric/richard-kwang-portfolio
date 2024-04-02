9f861280d8bd9e984b96ce239c07682779a70eee
Richard Kwang <43795498+kwangric@users.noreply.github.com>
2024-04-02 13:53:30 +0000

    Fix redirect maybe

--- pages/btpl.js
+++ pages/btpl.js
@@ -1,11 +1,9 @@
-import { useRouter } from 'next/router'
+import Router from 'next/router'
 import { useEffect } from 'react'
 
 const btpl = () => {
-    const router = useRouter()
-    const redirect = () => router.push('https://btpl-richard.vercel.app/')
     useEffect(() => {
-        redirect()
+        Router.push('https://btpl-richard.vercel.app/')
     }, [])
     return (<>
 
